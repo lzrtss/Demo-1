925d5f81822ec42a7b51afff06ff3c01e3abe4a8
SergeiL <sergei.lazarets@gmail.com>
2019-09-17 09:55:47 +0300

    Added commas after last prop of each object (according to Airbnb code convention)

--- src/tests/test5.js
+++ src/tests/test5.js
@@ -6,26 +6,36 @@ export default function makeTests5() {
 
     describe('Tests with valid arguments', () => {
 
-      it('Success: \'Complex Method\' won ({ min: 1, max: 1000})', () => {
-        chai.assert.deepEqual(task5({ min: 1, max: 1000 }),
-          { winMethod: 'Complex Method', simpleCount: 0, complexCount: 54 });
+      it('Success: \'Complex Method\' won, simpleCount - 0, complexCount - 54 ({ min: 1, max: 1000, })', () => {
+        chai.assert.deepEqual(task5({ min: 1, max: 1000, }),
+          { winMethod: 'Complex Method', simpleCount: 0, complexCount: 54, });
       });
 
-      it('Success: \'Draw\' ({ min: 0, max: 999999 })', () => {
-        chai.assert.deepEqual(task5({ min: 0, max: 999999 }),
-          { winMethod: 'Draw', simpleCount: 55252, complexCount: 55252 });
+      it('Success: \'Complex Method\' won, simpleCount - 0, complexCount - 54 ({ min: 125, max: 2570, })', () => {
+        chai.assert.deepEqual(task5({ min: 125, max: 2570, }),
+          { winMethod: "Complex Method", simpleCount: 9, complexCount: 154, });
       });
 
-      it('Success: \'Simple Method\' won ({ min: 123456, max: 654321 })', () => {
-        chai.assert.deepEqual(task5({ min: 123456, max: 654321 }),
-          { winMethod: 'Simple Method', simpleCount: 31607, complexCount: 30213 });
+      it('Success: \'Draw\', simpleCount - 55252, complexCount - 55252 ({ min: 0, max: 999999, })', () => {
+        chai.assert.deepEqual(task5({ min: 0, max: 999999, }),
+          { winMethod: 'Draw', simpleCount: 55252, complexCount: 55252, });
+      });
+
+      it('Success: \'Simple Method\' won, simpleCount - 31607, complexCount - 30213 ({ min: 123456, max: 654321, })', () => {
+        chai.assert.deepEqual(task5({ min: 123456, max: 654321, }),
+          { winMethod: 'Simple Method', simpleCount: 31607, complexCount: 30213, });
+      });
+
+      it('Success: \'Simple Method\' won, simpleCount - 1638, complexCount - 1497 ({  min: 37505, max: 68240, })', () => {
+        chai.assert.deepEqual(task5({ min: 37505, max: 68240, }),
+          { winMethod: "Simple Method", simpleCount: 1638, complexCount: 1497, });
       });
 
     });
 
     describe('Tests with invalid or missing arguments', () => {
 
-      it('Error: Throws \'Invalid or missing arguments\' error - using String as \'min\' value: { min: \'100\', max: 5000 }', () => {
+      it('Error: Throws \'Invalid or missing arguments\' error - using String as \'min\' value: { min: \'100\', max: 5000, }', () => {
         chai.assert.deepEqual(task5('100', 5000),
           {
             status: 'failed',
@@ -33,7 +43,7 @@ export default function makeTests5() {
           });
       });
 
-      it('Error: Throws \'Invalid or missing arguments\' error - using NaN as \'max\' value: { min: 100, max: NaN }', () => {
+      it('Error: Throws \'Invalid or missing arguments\' error - using NaN as \'max\' value: { min: 100, max: NaN, }', () => {
         chai.assert.deepEqual(task5(100, NaN),
           {
             status: 'failed',
@@ -49,16 +59,16 @@ export default function makeTests5() {
           });
       });
 
-      it('Error: Throws \'Invalid or missing arguments\' error - missing \'min\' property: { max: 555555 }', () => {
-        chai.assert.deepEqual(task5({ max: 555555 }),
+      it('Error: Throws \'Invalid or missing arguments\' error - missing \'min\' property: { max: 555555, }', () => {
+        chai.assert.deepEqual(task5({ max: 555555, }),
           {
             status: 'failed',
             reason: 'Invalid or missing argument(s): Argument is object with 2 properties (min, max). For values of both properties only integers between 0 and 999999 are allowed. \'Max\' value should be greater or equal to \'min\'.',
           });
       });
 
-      it('Error: Throws \'Invalid or missing arguments\' error - missing \'max\' property: { min: 123456 }', () => {
-        chai.assert.deepEqual(task5({ min: 123456 }),
+      it('Error: Throws \'Invalid or missing arguments\' error - missing \'max\' property: { min: 123456, }', () => {
+        chai.assert.deepEqual(task5({ min: 123456, }),
           {
             status: 'failed',
             reason: 'Invalid or missing argument(s): Argument is object with 2 properties (min, max). For values of both properties only integers between 0 and 999999 are allowed. \'Max\' value should be greater or equal to \'min\'.',
@@ -73,32 +83,32 @@ export default function makeTests5() {
           });
       });
 
-      it('Error: Throws \'Invalid or missing arguments\' error - using negative number as \'min\' value: { min: -5, max: 25 }', () => {
-        chai.assert.deepEqual(task5({ min: -5, max: 25 }),
+      it('Error: Throws \'Invalid or missing arguments\' error - using negative number as \'min\' value: { min: -5, max: 25, }', () => {
+        chai.assert.deepEqual(task5({ min: -5, max: 25, }),
           {
             status: 'failed',
             reason: 'Invalid or missing argument(s): Argument is object with 2 properties (min, max). For values of both properties only integers between 0 and 999999 are allowed. \'Max\' value should be greater or equal to \'min\'.',
           });
       });
 
-      it('Error: Throws \'Invalid or missing arguments\' error - using float number as \'max\' value: { min: 15, max: 25.5 }', () => {
-        chai.assert.deepEqual(task5({ min: 15, max: 25.5 }),
+      it('Error: Throws \'Invalid or missing arguments\' error - using float number as \'max\' value: { min: 15, max: 25.5, }', () => {
+        chai.assert.deepEqual(task5({ min: 15, max: 25.5, }),
           {
             status: 'failed',
             reason: 'Invalid or missing argument(s): Argument is object with 2 properties (min, max). For values of both properties only integers between 0 and 999999 are allowed. \'Max\' value should be greater or equal to \'min\'.',
           });
       });
 
-      it('Error: Throws \'Invalid or missing arguments\' error - using number out of range (0-999999): { min: 5, max: 99999999 }', () => {
-        chai.assert.deepEqual(task5({ min: 5, max: 99999999 }),
+      it('Error: Throws \'Invalid or missing arguments\' error - using number out of range (0-999999): { min: 5, max: 99999999, }', () => {
+        chai.assert.deepEqual(task5({ min: 5, max: 99999999, }),
           {
             status: 'failed',
             reason: 'Invalid or missing argument(s): Argument is object with 2 properties (min, max). For values of both properties only integers between 0 and 999999 are allowed. \'Max\' value should be greater or equal to \'min\'.',
           });
       });
 
-      it('Error: Throws \'Invalid or missing arguments\' error - \'min\' > \'max\': { min: 11111, max: 222 }', () => {
-        chai.assert.deepEqual(task5({ min: 11111, max: 222 }),
+      it('Error: Throws \'Invalid or missing arguments\' error - \'min\' > \'max\': { min: 11111, max: 222, }', () => {
+        chai.assert.deepEqual(task5({ min: 11111, max: 222, }),
           {
             status: 'failed',
             reason: 'Invalid or missing argument(s): Argument is object with 2 properties (min, max). For values of both properties only integers between 0 and 999999 are allowed. \'Max\' value should be greater or equal to \'min\'.',
